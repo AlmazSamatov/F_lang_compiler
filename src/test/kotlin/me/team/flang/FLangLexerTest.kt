465cb4681486f011d12724fb8c67035361f52a19
DrompiX <mrlocky98@gmail.com>
2018-11-25 00:01:56 +0300

    Update FLangLexerTest.kt

--- src/test/kotlin/me/team/flang/FLangLexerTest.kt
+++ src/test/kotlin/me/team/flang/FLangLexerTest.kt
@@ -63,29 +63,3 @@ class FLangLexerTest {
             "LINE_FUN", "ID", "PLUS", "INT_LIT", "EOF"),
             tokens(lexerForCode("inc is func(p: integer) => p+1")))
     }
-
-//    @test fun parseVarDeclarationAssignedAnIntegerLiteral() {
-//        assertEquals(listOf("VAR", "ID", "ASSIGN", "INTLIT", "SEMI", "EOF"),
-//            tokens(lexerForCode("var a = 1;")))
-//    }
-//
-//    @test fun parseVarDeclarationAssignedADecimalLiteral() {
-//        assertEquals(listOf("VAR", "ID", "ASSIGN", "DECLIT", "EOF"),
-//            tokens(lexerForCode("var a = 1.23")))
-//    }
-//
-//    @test fun parseVarDeclarationAssignedASum() {
-//        assertEquals(listOf("VAR", "ID", "ASSIGN", "INTLIT", "PLUS", "INTLIT", "EOF"),
-//            tokens(lexerForCode("var a = 1 + 2")))
-//    }
-//
-//    @test fun parseMathematicalExpression() {
-//        assertEquals(listOf("INTLIT", "PLUS", "ID", "ASTERISK", "INTLIT", "DIVISION", "INTLIT", "MINUS", "INTLIT", "EOF"),
-//            tokens(lexerForCode("1 + a * 3 / 4 - 5")))
-//    }
-//
-//    @test fun parseMathematicalExpressionWithParenthesis() {
-//        assertEquals(listOf("INTLIT", "PLUS", "LPAREN", "ID", "ASTERISK", "INTLIT", "RPAREN", "MINUS", "DECLIT", "EOF"),
-//            tokens(lexerForCode("1 + (a * 3) - 5.12")))
-//    }
-}
