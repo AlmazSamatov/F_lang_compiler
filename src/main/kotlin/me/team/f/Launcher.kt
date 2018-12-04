5d56c9122bd7900d0a33b690b4b8af335674fa08
AlmazSamatov <samatov.almaz@yandex.ru>
2018-12-04 15:26:08 +0300

    changed launcher

--- src/main/kotlin/me/team/f/Launcher.kt
+++ src/main/kotlin/me/team/f/Launcher.kt
@@ -5,12 +5,15 @@ import me.team.f.parsing.Analyser
 
 fun main(args: Array<String>) {
     val caller = Caller()
-//    println(caller.getTokens(caller.lexerForCode("a is 5; b is a^1;")))
-//        println(toParseTree(caller.parseFile("simple")).getString())
-    //val code = """l = 0;""".trimMargin()
-    //print(Analyser.parse(code))
 
-    val code = """a is if 5 > 1 then 5 else 1""".trimMargin()
+//    val code = """
+//            inc is func(p: integer): integer => p+1
+//        """.trimIndent()
+    val code = """
+            b is if a = true then a := true else a := false end
+        """.trimIndent()
     val ast = Analyser.parse(code).root!!.toAst()
     println(ast)
 }
+
+
