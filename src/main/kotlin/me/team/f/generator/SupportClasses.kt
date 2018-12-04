228a9816b2d986d23cbbd9acb00df95209be9a6d
Vasile Rotaru <vasilerotaru38@gmail.com>
2018-12-04 17:30:39 +0300

    Added Validator and JVM Compiler. They are incomplete.

--- src/main/kotlin/me/team/f/generator/SupportClasses.kt
+++ src/main/kotlin/me/team/f/generator/SupportClasses.kt
@@ -1,8 +1,14 @@
 package me.team.f.generator
 
-import kotlin.math.hypot
+import kotlin.math.sqrt
 
 class Rational(var numerator: Int, var denominator: Int) {
+    init {
+        check(this.denominator == 0) {
+            "Denominator cannot be 0."
+        }
+    }
+
     operator fun plus(other: Rational): Rational {
         return Rational(
             this.numerator * other.denominator + this.denominator * other.numerator,
@@ -10,7 +16,18 @@ class Rational(var numerator: Int, var denominator: Int) {
         )
     }
 
+
     operator fun plusAssign(other: Rational) {
+        val aux = this + other
+        this.numerator = aux.numerator
+        this.denominator = aux.denominator
+    }
+
+    operator fun plus(other: Int): Rational {
+        return this + Rational(other, 1)
+    }
+
+    operator fun plusAssign(other: Int) {
         val aux = this + other
         this.numerator = aux.numerator
         this.denominator = aux.denominator
@@ -26,11 +43,31 @@ class Rational(var numerator: Int, var denominator: Int) {
         this.denominator = aux.denominator
     }
 
+    operator fun minus(other: Int): Rational {
+        return this + Rational(other, 1)
+    }
+
+    operator fun minusAssign(other: Int) {
+        val aux = this - other
+        this.numerator = aux.numerator
+        this.denominator = aux.denominator
+    }
+
     operator fun times(other: Rational): Rational {
         return Rational(this.numerator * other.numerator, this.denominator * other.denominator)
     }
 
     operator fun timesAssign(other: Rational) {
+        val aux = this * other
+        this.numerator = aux.numerator
+        this.denominator = aux.denominator
+    }
+
+    operator fun times(other: Int): Rational {
+        return Rational(this.numerator * other, this.denominator)
+    }
+
+    operator fun timesAssign(other: Int) {
         val aux = this * other
         this.numerator = aux.numerator
         this.denominator = aux.denominator
@@ -46,7 +83,20 @@ class Rational(var numerator: Int, var denominator: Int) {
         this.denominator = aux.denominator
     }
 
+    operator fun div(other: Int): Rational {
+        return this * Rational(1, other)
+    }
+
+    operator fun divAssign(other: Int) {
+        val aux = this / other
+        this.numerator = aux.numerator
+        this.denominator = aux.denominator
+    }
+
     fun inverse(): Rational {
+        check(this.numerator == 0) {
+            "Number 0 has no inverse."
+        }
         return Rational(this.denominator, this.numerator)
     }
 
@@ -59,9 +109,25 @@ class Rational(var numerator: Int, var denominator: Int) {
     }
 }
 
+operator fun Int.plus(other: Rational): Rational {
+    return other + this
+}
+
+operator fun Int.minus(other: Rational): Rational {
+    return Rational(this, 1) - other
+}
+
+operator fun Int.times(other: Rational): Rational {
+    return other * this
+}
+
+operator fun Int.div(other: Rational): Rational {
+    return this * other.inverse()
+}
+
 class Complex(var real: Double, var imaginary: Double) {
     fun modulus(): Double {
-        return hypot(this.real, this.imaginary)
+        return sqrt((this * this.conjugate()).real)
     }
 
     operator fun plus(other: Complex): Complex {
@@ -71,7 +137,27 @@ class Complex(var real: Double, var imaginary: Double) {
     operator fun plusAssign(other: Complex) {
         val aux = this + other
         this.real = aux.real
-        this.imaginary = other.imaginary
+        this.imaginary = aux.imaginary
+    }
+
+    operator fun plus(other: Double): Complex {
+        return (Complex(this.real + other, this.imaginary))
+    }
+
+    operator fun plusAssign(other: Double) {
+        val aux = this + other
+        this.real = aux.real
+        this.imaginary = aux.imaginary
+    }
+
+    operator fun plus(other: Int): Complex {
+        return (Complex(this.real + other, this.imaginary))
+    }
+
+    operator fun plusAssign(other: Int) {
+        val aux = this + other
+        this.real = aux.real
+        this.imaginary = aux.imaginary
     }
 
     operator fun minus(other: Complex): Complex {
@@ -81,7 +167,27 @@ class Complex(var real: Double, var imaginary: Double) {
     fun minusAssign(other: Complex) {
         val aux = this - other
         this.real = aux.real
-        this.imaginary = other.imaginary
+        this.imaginary = aux.imaginary
+    }
+
+    operator fun minus(other: Double): Complex {
+        return this + (-1.0) * other
+    }
+
+    fun minusAssign(other: Double) {
+        val aux = this - other
+        this.real = aux.real
+        this.imaginary = aux.imaginary
+    }
+
+    operator fun minus(other: Int): Complex {
+        return this + (-1.0) * other
+    }
+
+    fun minusAssign(other: Int) {
+        val aux = this - other
+        this.real = aux.real
+        this.imaginary = aux.imaginary
     }
 
     operator fun times(other: Complex): Complex {
@@ -94,11 +200,27 @@ class Complex(var real: Double, var imaginary: Double) {
     operator fun timesAssign(other: Complex) {
         val aux = this * other
         this.real = aux.real
-        this.imaginary = other.imaginary
+        this.imaginary = aux.imaginary
     }
 
     operator fun times(other: Double): Complex {
         return this * Complex(other, 0.0)
+    }
+
+    operator fun timesAssign(other: Double) {
+        val aux = this * other
+        this.real = aux.real
+        this.imaginary = aux.imaginary
+    }
+
+    operator fun times(other: Int): Complex {
+        return this * other.toDouble()
+    }
+
+    operator fun timesAssign(other: Int) {
+        val aux = this * other
+        this.real = aux.real
+        this.imaginary = aux.imaginary
     }
 
     operator fun div(other: Complex): Complex {
@@ -115,13 +237,60 @@ class Complex(var real: Double, var imaginary: Double) {
         return 1 / other * this
     }
 
+    operator fun divAssign(other: Double) {
+        val aux = this / other
+        this.real = aux.real
+        this.imaginary = aux.imaginary
+    }
+
+    operator fun div(other: Int): Complex {
+        return 1.0 / other * this
+    }
+
+    operator fun divAssign(other: Int) {
+        val aux = this / other
+        this.real = aux.real
+        this.imaginary = aux.imaginary
+    }
+
     fun inverse(): Complex {
+        check(this.real == 0.0 && this.imaginary == 0.0) {
+            "Number 0 has no inverse."
+        }
         return 1.0 / (this.modulus() * this.modulus()) * conjugate()
     }
 
     fun conjugate() = Complex(this.real, -1 * this.imaginary)
 }
 
-private operator fun Double.times(complex: Complex): Complex {
+operator fun Double.plus(other: Complex): Complex {
+    return other + this
+}
+
+operator fun Double.minus(other: Complex): Complex {
+    return Complex(this - other.real, other.imaginary)
+}
+
+operator fun Double.times(complex: Complex): Complex {
     return complex * this
+}
+
+operator fun Double.div(other: Complex): Complex {
+    return this * other.inverse()
+}
+
+operator fun Int.plus(other: Complex): Complex {
+    return other + this
+}
+
+operator fun Int.minus(other: Complex): Complex {
+    return Complex(this - other.real, other.imaginary)
+}
+
+operator fun Int.times(complex: Complex): Complex {
+    return complex * this
+}
+
+operator fun Int.div(other: Complex): Complex {
+    return this * other.inverse()
 }
