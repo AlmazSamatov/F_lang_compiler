461e7b1e4650c7c21c2ee980fd4d33104dd97562
Dima T <mrlocky98@gmail.com>
2018-12-07 16:01:11 +0300

    change

--- generated-src/compilation/gen_program.kt
+++ generated-src/compilation/gen_program.kt
@@ -1,35 +1,19 @@
 fun main(args: Array<String>) {
-	fun lengthOfArray (arr: List<Int>): Int{
-	var count = 0
+	var array: List<Int> = mutableListOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)
+	var max = array[0]
+	fun find_max (arr: List<Int>): Int{
 	for (i in arr) {
-	count = count + 1
+	if (i > max) {
+	max = i
+	} else {
 	} 
-	return count
-	}
-	var telephone_book: Map<Any, Any> = mutableMapOf("John Lord" to 1000125, "Ian Gillan" to 2200330)
-	var info: Map<Any, Any> = mapOf("name" to "John", "age" to 21)
-	var arr: List<Int> = mutableListOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-	fun printTuple (t: Map<Any, Any>){
-	for (e in t) {
-	print(e)
-	print(" ")
 	} 
-	print("\n")
-	}
-	fun printArray (array: List<Int>){
-	for (e in array) {
-	print(e)
-	print(" ")
-	} 
+	return max
 	}
 	fun main (){
-	print("Tuple elements: ")
-	printTuple (info) 
-	print("Array length is: ")
-	print(lengthOfArray(arr))
+	print("Max is ")
+	print(find_max(array))
 	print("\n")
-	print("Array elements are: ")
-	printArray (arr) 
 	}
-	var t = main()
+	var res = main()
 }
--- test_code.f
+++ test_code.f
@@ -1,40 +1,18 @@
-lengthOfArray is func(arr: [integer]): integer do
-    count is 0
+array is [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 
+max is array[0];
+
+find_max is func(arr: [integer]) : integer do
     for i in arr loop
-        count := count + 1
+        if i > max then
+            max := i
+        end
     end
-
-    return count
+    return max
 end;
 
-telephone_book is { "John Lord": 1000125, "Ian Gillan": 2200330 };
-
-info is (name is "John", age is 21);
-
-arr is [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-printTuple is func(t:(string,integer)) do
-     for e in t loop
-        print e, " "
-     end
-     print "\n"
+main is func() do
+    print "Max is ", find_max(array), "\n"
 end;
 
-printArray is func(array: [integer]) do
-     for e in array loop
-        print e, " "
-     end
-end;
-
-main is func()
-do
-     print "Tuple elements: "
-     printTuple(info)
-     print "Array length is: ", lengthOfArray(arr), "\n"
-     print "Array elements are: "
-     printArray(arr)
-
-end;
-
-t is main()
+res is main()
